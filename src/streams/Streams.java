14464d84a9bb4ab0745ceb39566ca2761090cd4e
Paul2803k <p.scattolin95@gmail.com>
2019-05-18 21:46:48 +0200

    names and sds

--- src/streams/Streams.java
+++ src/streams/Streams.java
@@ -1,7 +1,9 @@
 package streams;
-
 /* NOTHING TO SEE HERE */
-
+/**
+ * @author Paolo Scattolin s1023775
+ * @author Johan Urban s1024726
+ */
 public class Streams {
     public static void main(String[] args) {
     }
